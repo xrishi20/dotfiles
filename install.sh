1000216ee665f6201cf59a0175aae1ff181d7c9b
Rishi <xrishi20@gmail.com>
2026-06-21 17:03:04 +0530

    Simplify

--- install.sh
+++ install.sh
@@ -6,29 +6,26 @@ set -e
 # Define source directories
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONFIG_DIR="$DOTFILES_DIR/config"
-FONTS_DIR="$DOTFILES_DIR/fonts"
 
 # Define target directories
 USER_CONFIG="$HOME/.config"
-SYSTEM_FONTS="/usr/share/fonts"
 
-. ./scripts/base.sh
-# . ./scripts/gaming.sh
+# Installing Pckages
+echo "Installing Base Packages"
+yay -S --needed hyprland sddm awww hyprpicker hyprlock xdg-desktop-portal-hyprland xdg-desktop-portal-gtk git nano kitty uwsm ddcutil xdg-user-dirs xdg-user-dirs-gtk waybar openssh tofi dunst gnome-font-viewer unzip gvfs-mtp android-tools android-file-transfer android-udev libmtp fish fisher starship nautilus udev-block-notify htop polkit-gnome gnome-keyring seahorse udisks2 brightnessctl bluez bluez-utils blueman qt5-wayland qt6-wayland pipewire wireplumber pipewire-pulse playerctl pavucontrol nm-connection-editor grim slurp wl-clipboard inter-font ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols noto-fonts-emoji papirus-icon-theme sddm-sugar-candy-git papirus-folders helium-browser-bin zed
 
+# Copy Configs
 echo "Copying Configs"
 mkdir -p "$USER_CONFIG"
 cp -r "$CONFIG_DIR"/* "$USER_CONFIG/"
 
-echo "Installing Dank Mono"
-sudo mkdir -p "$SYSTEM_FONTS"
-sudo cp -r "$FONTS_DIR"/* "$SYSTEM_FONTS/"
-sudo fc-cache -fv  # Update font cache
-
+# Git
 echo "Configure Git aliases"
 git config --global alias.a "add ."
 git config --global alias.cm "commit -m"
 git config --global alias.puo "push -u origin"
 
+# SDDM
 echo "Configure SDDM theme"
 sudo bash -c 'cat <<EOF >> /etc/sddm.conf
 
--- scripts/gaming.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Installing Steam & Proton GE"
-yay -S --needed steam gamemode gamescope mangohud proton-ge-custom-bin
