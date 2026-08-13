cd9c2276ca4b9d82b1b4b0c00696e904b5b4326a
Rishi <xrishi20@gmail.com>
2026-08-13 12:24:58 +0530

    Remove noconfirm flag, reboot at the end

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ USER_CONFIG="$HOME/.config"
 
 # Installing Pckages
 echo "Installing Base Packages"
-yay -S --needed --noconfirm hyprland sddm awww hyprpicker xdg-desktop-portal-hyprland xdg-desktop-portal-gtk git nano kitty uwsm ddcutil xdg-user-dirs xdg-user-dirs-gtk waybar openssh tofi dunst gnome-font-viewer unzip gvfs-mtp android-tools android-file-transfer android-udev libmtp fish fisher starship nautilus htop polkit-gnome gnome-keyring seahorse udisks2 brightnessctl bluez bluez-utils blueman qt5-wayland qt6-wayland pipewire wireplumber pipewire-pulse playerctl pavucontrol nm-connection-editor grim slurp wl-clipboard inter-font ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols noto-fonts-emoji papirus-icon-theme sddm-sugar-candy-git papirus-folders helium-browser-bin zed deno
+yay -S --needed hyprland sddm awww hyprpicker xdg-desktop-portal-hyprland xdg-desktop-portal-gtk git nano kitty uwsm ddcutil xdg-user-dirs xdg-user-dirs-gtk waybar openssh tofi dunst gnome-font-viewer unzip gvfs-mtp android-tools android-file-transfer android-udev libmtp fish fisher starship nautilus htop polkit-gnome gnome-keyring seahorse udisks2 brightnessctl bluez bluez-utils blueman qt5-wayland qt6-wayland pipewire wireplumber pipewire-pulse playerctl pavucontrol nm-connection-editor grim slurp wl-clipboard inter-font ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols noto-fonts-emoji papirus-icon-theme sddm-sugar-candy-git papirus-folders helium-browser-bin zed deno localsend-bin
 
 # Copy Configs
 echo "Copying Configs"
@@ -51,4 +51,8 @@ echo "Setting Fish as default shell"
 chsh -s /usr/bin/fish
 
 # Enable SDDM
-sudo systemctl enable --now sddm
+echo "Enable SDDM"
+sudo systemctl enable sddm
+
+echo "Rebooting Now"
+sudo reboot now
