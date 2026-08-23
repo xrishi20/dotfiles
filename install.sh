fd02ee47e3abca2c521fe76280fb1116c55057e3
Rishi <xrishi20@gmail.com>
2026-08-23 16:25:52 +0530

    Simplify

--- install.sh
+++ install.sh
@@ -14,45 +14,49 @@ rm -rf yay-bin
 # Define source directories
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONFIG_DIR="$DOTFILES_DIR/config"
+LOCAL_DIR="$DOTFILES_DIR/local"
 
 # Define target directories
 USER_CONFIG="$HOME/.config"
+USER_LOCAL="$HOME/.local"
+
+# Dirs
+echo "Created Basic Directories"
+mkdir -p Documents Downloads Videos Projects Pictures/Wallpapers
 
 # Installing Pckages
-echo "Installing Base Packages"
-yay -S --needed hyprland sddm awww hyprpicker xdg-desktop-portal-hyprland xdg-desktop-portal-gtk git nano kitty uwsm ddcutil xdg-user-dirs xdg-user-dirs-gtk waybar openssh tofi dunst gnome-font-viewer unzip gvfs-mtp android-tools android-file-transfer android-udev libmtp fish fisher starship nautilus htop polkit-gnome gnome-keyring seahorse udisks2 brightnessctl bluez bluez-utils blueman qt5-wayland qt6-wayland pipewire wireplumber pipewire-pulse playerctl pavucontrol nm-connection-editor grim slurp wl-clipboard inter-font ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols noto-fonts-emoji papirus-icon-theme sddm-sugar-candy-git papirus-folders helium-browser-bin zed deno localsend-bin
+echo "Installing Packages"
+yay -S --needed niri xwayland-satellite greetd noctalia noctalia-greeter xdg-desktop-portal-gtk git nano kitty ddcutil pastel wl-clipboard xdg-user-dirs xdg-user-dirs-gtk openssh unzip gvfs-mtp android-tools android-file-transfer android-udev libmtp fish starship nautilus udisks2 brightnessctl bluez bluez-utils qt5-wayland qt6-wayland pipewire wireplumber pipewire-pulse playerctl pavucontrol nm-connection-editor inter-font ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols noto-fonts-emoji papirus-icon-theme zed deno helium-browser-bin localsend-bin
 
-# Copy Configs
+# Configs
 echo "Copying Configs"
 mkdir -p "$USER_CONFIG"
+mkdir -p "$USER_LOCAL"
 cp -r "$CONFIG_DIR"/* "$USER_CONFIG/"
+cp -r "$LOCAL_DIR"/* "$USER_LOCAL/"
 
 # Git
-echo "Configure Git aliases"
+echo "Configured Git aliases"
 git config --global alias.a "add ."
 git config --global alias.cm "commit -m"
 git config --global alias.puo "push -u origin"
 
-# SDDM
-echo "Configure SDDM theme"
-sudo bash -c 'cat <<EOF >> /etc/sddm.conf
+# Greeter
+echo "Configured Greetd"
+sudo bash -c 'cat <<EOF > /etc/greetd/config.conf
 
-[Theme]
-Current = sugar-candy
-CursorTheme = Vision
+[terminal]
+vt = 1
+
+[default_session]
+command = "/usr/bin/noctalia-greeter-session -- --session niri"
+user = "greeter"
 EOF'
-
-# Papirus Folders
-echo "Setting Papirus Folders theme"
-papirus-folders -C cyan
 
 # Fish
 echo "Setting Fish as default shell"
 chsh -s /usr/bin/fish
 
-# Enable SDDM
-echo "Enable SDDM"
-sudo systemctl enable sddm
-
-echo "Rebooting Now"
-sudo reboot now
+# Enable Greetd
+echo "Enable Greetd"
+sudo systemctl enable --now greetd
---
+++ local/share/icons/Vision/README.txt
@@ -0,0 +1,12 @@
+Installation
+```bash
+tar -xvf visioncursorwhite.tar.gz    	       # extract `visioncursorwhite.tar.gz`
+mv visioncursorwhite ~/.icons/        	       # Install to local users
+sudo mv visioncursorwhite /usr/share/icons/    # Install to all users
+```
+
+Uninstallation
+```bash
+rm ~/.icons/visioncursorwhite                  # Remove from local users
+sudo rm /usr/share/icons/visioncursorwhite     # Remove from all users
+```
