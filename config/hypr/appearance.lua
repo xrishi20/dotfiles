63d8794561687148cbc8cbe2533a64723c706142
Rishi <xrishi20@gmail.com>
2026-08-28 20:22:11 +0530

    Back to Hyprland :}

---
+++ config/hypr/appearance.lua
@@ -0,0 +1,87 @@
+-- Monitors
+hl.monitor({ output = "DP-3", mode = "2560x1440@180", position = "0x0", scale = 1 })
+hl.monitor({ output = "eDP-1", mode = "1920x1200@60", position = "2560x0", scale = 1 })
+
+-- Assign Workspaces to Monitors
+for i = 1, 2 do
+   hl.workspace_rule({ workspace = i, monitor = "DP-3" })
+end
+
+for i = 3, 9 do
+   hl.workspace_rule({ workspace = i, monitor = "eDP-1" })
+end
+
+-- Theming
+hl.on("hyprland.start", function()
+   hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme prefer-dark")
+   hl.exec_cmd("gsettings set org.gnome.desktop.interface font-antialiasing rgba")
+   hl.exec_cmd("gsettings set org.gnome.desktop.interface font-hinting slight")
+   hl.exec_cmd("gsettings set org.gnome.desktop.interface icon-theme 'Papirus'")
+   hl.exec_cmd("gsettings set org.gnome.desktop.interface font-name 'Inter'")
+   hl.exec_cmd("gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMono NF'")
+   hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'Vision'")
+   hl.exec_cmd("hyprctl setcursor Vision 24")
+end)
+
+-- Decoration
+hl.config({
+   general = {
+      gaps_in          = 2,
+      gaps_out         = 8,
+
+      border_size      = 2,
+
+      col              = {
+         active_border   = "rgb(33ccff)",
+         inactive_border = "rgba(ffffff40)",
+      },
+
+      resize_on_border = false,
+      allow_tearing    = false,
+
+      layout           = "dwindle",
+   },
+
+   decoration = {
+      rounding         = 10,
+      rounding_power   = 2,
+
+      active_opacity   = 1.0,
+      inactive_opacity = 1.0,
+
+      shadow           = {
+         enabled = false,
+      },
+
+      blur             = {
+         enabled  = true,
+         size     = 3,
+         passes   = 3,
+         vibrancy = 0.125,
+      },
+   },
+
+   animations = {
+      enabled = true,
+   },
+})
+
+-- Misc
+hl.config({
+   misc = {
+      force_default_wallpaper = -1,
+      disable_hyprland_logo   = true,
+   },
+})
+
+-- Window Rules
+hl.window_rule({
+   name = "fileManager",
+   match = {
+      class = "(xdg-desktop-portal-gtk)",
+   },
+   move = { "window_w * 0.5", "monitor_h * 0.25" },
+   size = { "monitor_w * 0.5", "monitor_h * 0.5" },
+   float = true,
+   stay_focused = true
+})
--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ mkdir -p Documents Downloads Videos Projects Pictures/Wallpapers
 
 # Installing Pckages
 echo "Installing Packages"
-yay -S --needed niri xwayland-satellite greetd noctalia noctalia-greeter xdg-desktop-portal-gtk git nano kitty ddcutil pastel wl-clipboard xdg-user-dirs xdg-user-dirs-gtk openssh unzip gvfs-mtp android-tools android-file-transfer android-udev libmtp fish starship nautilus udisks2 brightnessctl bluez bluez-utils qt5-wayland qt6-wayland pipewire wireplumber pipewire-pulse playerctl pavucontrol nm-connection-editor inter-font ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols noto-fonts-emoji papirus-icon-theme zed deno helium-browser-bin localsend-bin
+yay -S --needed hyprland xdg-desktop-portal-hyprland hyprpicker grim slurp greetd noctalia noctalia-greeter xdg-desktop-portal-gtk git nano kitty ddcutil pastel wl-clipboard xdg-user-dirs xdg-user-dirs-gtk openssh unzip gvfs-mtp android-tools android-file-transfer android-udev libmtp fish starship nautilus udisks2 brightnessctl bluez bluez-utils qt5-wayland qt6-wayland pipewire wireplumber pipewire-pulse playerctl pavucontrol nm-connection-editor inter-font ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols noto-fonts-emoji papirus-icon-theme zed deno helium-browser-bin localsend-bin
 
 # Configs
 echo "Copying Configs"
@@ -49,7 +49,7 @@ sudo bash -c 'cat <<EOF > /etc/greetd/config.conf
 vt = 1
 
 [default_session]
-command = "/usr/bin/noctalia-greeter-session -- --session niri"
+command = "/usr/bin/noctalia-greeter-session -- --session hyprland"
 user = "greeter"
 EOF'
 
